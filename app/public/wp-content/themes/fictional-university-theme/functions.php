097aff198e0eb8c0989d600bf3b7f7fab240ad8b
Mitch Brunke <mbrunke@fcte.com.au>
2021-01-21 16:16:51 +1000

    WP Menu

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -10,6 +10,9 @@ function university_files() {
 add_action('wp_enqueue_scripts', 'university_files'); 
 
 function university_features() {
+    register_nav_menu('headerMenu', 'Header Menu');
+    register_nav_menu('footerLocationOne', 'footer Location One');
+    register_nav_menu('footerLocationTwo', 'footer Location Two');
     add_theme_support('title-tag');
 }
 
--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -17,25 +17,48 @@ while(have_posts()) {
 
   <div class="container container--narrow page-section">
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+<?php 
+$theParentID = wp_get_post_parent_id(get_the_ID());
+  if($theParentID) { ?>
+
+  <div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParentID) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParentID) ?></a> <span class="metabox__main"><?php echo the_title(); ?></span></p>
     </div>
+
+  <?php  
+  }
+?>
     
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+<?php 
+$testArray = get_pages(array(
+  'child_of' => get_the_ID()
+)); 
+
+if($theParentID or $testArray) { ?>
+      <div class="page-links">
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($theParentID) ?>"><?php echo get_the_title($theParentID) ?></a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+        <?php 
+      if( $theParentID ) {
+        $findChildren = $theParentID;
+      } else {
+        $findChildren = get_the_ID(); 
+      }
+          echo wp_list_pages(array(
+           'title_li' => NULL,
+           'child_of' => $findChildren,
+           'sort_column' => 'menu_order'
+          )); 
+        ?>
       </ul>
-    </div> -->
+    </div>  
+<?php } ?>
 
     <div class="generic-content">
       <?php the_content(); ?> 
     </div>
 
   </div>
-
-
  <?php  }
 
 get_footer(); 
